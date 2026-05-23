3d95356395ec078d431f38cc071e26df146e441f
ahmmatt <basyirsinjai@gmail.com>
2026-05-23 20:18:13 +0800

    Implement validation, error handling & service layer

---
+++ app/Exceptions/Handler.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Foundation\Application;
+use Illuminate\Foundation\Configuration\Exceptions;
+use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
+use Throwable;
+
+return Application::configure(basePath: dirname(__DIR__))
+    ->withRouting(
+        web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/api.php',
+        commands: __DIR__.'/../routes/console.php',
+        health: '/up',
+    )
+    ->withMiddleware(function (Middleware $middleware) {
+        //
+    })
+    ->withExceptions(function (Exceptions $exceptions) {
+        // Penyesuaian Global Exception dari Modul 3
+        $exceptions->render(function (Throwable $e, Request $request) {
+            if ($request->is('api/*')) {
+                $statusCode = method_exists($e, 'getStatusCode') ? $e->getStatusCode() : 500;
+                return response()->json([
+                    'status' => 'error',
+                    'data' => null,
+                    'message' => $e->getMessage(),
+                ], $statusCode);
+            }
+        });
+    })->create();
---
+++ app/Http/Requests/UpdateCategoryRequest.php
@@ -0,0 +1,16 @@
+<?php
+namespace App\Http\Requests;
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateCategoryRequest extends FormRequest {
+    public function authorize() {
+        return true;
+    }
+
+    public function rules() {
+        $id = $this->route('category'); // Mengambil ID dari URL
+        return [
+            'name' => "required|string|unique:categories,name,{$id}"
+        ];
+    }
+}
